b865a5c95573fc5b9f54133088651128072ca852
Slapicola <slapicola@byupathway.edu>
2026-07-11 19:33:30 -0400

    Completed Week 2 Code Assignment

--- week02/code/PriorityQueue_Tests.cs
+++ week02/code/PriorityQueue_Tests.cs
@@ -6,23 +6,63 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 public class PriorityQueueTests
 {
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
+    // Scenario: Create a queue where the high priority number is at the end. A (10), B (9), C(7), D(10)
+    // Expected Result: D, B, C, A
+    // Defect(s) Found: The Dequeue Method never removed items from the queue.
     public void TestPriorityQueue_1()
     {
+
+        var a = new PriorityItem("A", 1);
+        var b = new PriorityItem("B", 9);
+        var c = new PriorityItem("C", 7);
+        var d = new PriorityItem("D", 10);
+
+        PriorityItem[] expectedResult = [d, b, c, a];
+
         var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+        priorityQueue.Enqueue(a.Value, a.Priority);
+        priorityQueue.Enqueue(b.Value, b.Priority);
+        priorityQueue.Enqueue(c.Value, c.Priority);
+        priorityQueue.Enqueue(d.Value, d.Priority);
+
+            int i = 0;
+        while (priorityQueue.Length > 0)
+        {
+            var dequeuedLetter = priorityQueue.Dequeue();
+            Assert.AreEqual(expectedResult[i].Value, dequeuedLetter);
+            i++;
+        }
+        Assert.AreEqual(expectedResult.Length, i);
+
     }
 
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
+    // Scenario: Create a queue where there are multiple data values with high priority. A(9), B(10), C(7), D(10) 
+    // Expected Result: B, D, A, C
     // Defect(s) Found: 
     public void TestPriorityQueue_2()
     {
+        var a = new PriorityItem("A", 9);
+        var b = new PriorityItem("B", 10);
+        var c = new PriorityItem("C", 7);
+        var d = new PriorityItem("D", 10);
+
+        PriorityItem[] expectedResult = [b, d, a, c];
+
         var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+        priorityQueue.Enqueue(a.Value, a.Priority);
+        priorityQueue.Enqueue(b.Value, b.Priority);
+        priorityQueue.Enqueue(c.Value, c.Priority);
+        priorityQueue.Enqueue(d.Value, d.Priority);
+
+            int i = 0;
+        while (priorityQueue.Length > 0)
+        {
+            var dequeuedLetter = priorityQueue.Dequeue();
+            Assert.AreEqual(expectedResult[i].Value, dequeuedLetter);
+            i++;
+        }
+        Assert.AreEqual(expectedResult.Length, i);
     }
 
     // Add more test cases as needed below.
